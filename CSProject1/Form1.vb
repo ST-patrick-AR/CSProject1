547e2725943f7a52f9cd34db5a336376de1059a4
REF2024 <REF2024@PL32>
2024-11-15 12:10:19 +0800

    Add project files.

---
+++ CSProject1/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Private Sub Button1_connect_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=test"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to database was successful!")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Button2_login_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=test"
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "SELECT * FROM Accounts WHERE username = '" & tbUN.Text & "' AND password = '" & tbPASS.Text & "'"
+            COMMAND = New MySqlCommand(Query, conn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+            While READER.Read
+                count = count + 1
+            End While
+            If count = 1 Then
+                MessageBox.Show("Login successful!")
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and password are duplicate!")
+            Else
+                MessageBox.Show("Username and password are incorrect!")
+            End If
+
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+End Class
